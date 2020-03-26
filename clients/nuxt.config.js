34b1a18ef8bee99fd4b8616406b2883148a7a817
kuysomrith@gmail.com <kuysomrith@gmail.com>
2020-03-26 14:08:42 +0700

    add

--- clients/nuxt.config.js
+++ clients/nuxt.config.js
@@ -1,82 +1,65 @@
-const pkg = require('./package')
 
-
-const VuetifyLoaderPlugin = require('vuetify-loader/lib/plugin')
-
-module.exports = {
+export default {
   mode: 'spa',
-
   /*
   ** Headers of the page
   */
   head: {
-    title: 'Vue Material Admin Template',
+    title: process.env.npm_package_name || '',
     meta: [
-      {charset: 'utf-8'},
-      {name: 'viewport', content: 'width=device-width, initial-scale=1'},
-      {
-        hid: 'description', name: 'description', content: 'Vue Material Admin Template is a \n' +
-          '    Google Material Design inspired admin dashboard template built with Vue and Vuetify.'
-      }
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
     ],
     link: [
-      {rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'},
-      {
-        rel: 'stylesheet',
-        href:
-          'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons'
-      }
-    ],
-    script: [
-      {src: 'https://cdnjs.cloudflare.com/ajax/libs/echarts/4.0.4/echarts-en.min.js'}
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
     ]
   },
-
   /*
   ** Customize the progress-bar color
   */
-  loading: {color: '#3adced'},
-
+  loading: { color: '#fff' },
   /*
   ** Global CSS
   */
   css: [
-    '~/assets/style/theme.styl',
-    '~/assets/style/app.styl',
-    'font-awesome/css/font-awesome.css',
-    'roboto-fontface/css/roboto/roboto-fontface.css'
   ],
-
   /*
   ** Plugins to load before mounting the App
   */
   plugins: [
-    '@/plugins/vuetify',
-    '@/plugins/vee-validate'
   ],
-
+  /*
+  ** Nuxt.js dev-modules
+  */
+  buildModules: [
+    // Doc: https://github.com/nuxt-community/eslint-module
+    '@nuxtjs/eslint-module',
+  ],
   /*
   ** Nuxt.js modules
   */
-  modules: [],
-
+  modules: [
+    // Doc: https://axios.nuxtjs.org/usage
+    '@nuxtjs/axios',
+    '@nuxtjs/pwa',
+    // Doc: https://github.com/nuxt-community/dotenv-module
+    '@nuxtjs/dotenv',
+  ],
+  /*
+  ** Axios module configuration
+  ** See https://axios.nuxtjs.org/options
+  */
+  axios: {
+  },
   /*
   ** Build configuration
   */
   build: {
-    transpile: ['vuetify/lib'],
-    plugins: [new VuetifyLoaderPlugin()],
-    loaders: {
-      stylus: {
-        import: ["~assets/style/variables.styl"]
-      }
-    },
-
     /*
     ** You can extend webpack config here
     */
-    extend(config, ctx) {
-
+    extend (config, ctx) {
     }
   }
 }
