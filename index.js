bd6b07cd99ae35c5f3a90d8a9056c2f6edf7db6a
kuysomrith@gmail.com <kuysomrith@gmail.com>
2020-03-26 12:31:03 +0700

    add file

--- index.js
+++ index.js
@@ -27,5 +27,5 @@ app.get("/", (req, res) => {
 // Listen on port 5000
 app.listen(port, () => {
   console.log(`Server is booming on port 5000
-Visit http://localhost:5000`);
+Visit http://localhost:5000. Yeah OK`);
 });
